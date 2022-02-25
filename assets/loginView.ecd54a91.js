cf4c5a79bacf6f815ed3ba22f2fedb5a833dcf32
connectshark <46649021+connectshark@users.noreply.github.com>
2022-02-25 06:27:25 +0000

    Deploying to gh-pages from @ connectshark/bitly-sample-app@31b33b1a4c8888185136487d7e05d65d303c0039 🚀

---
+++ assets/loginView.ecd54a91.js
@@ -0,0 +1,1 @@
+import{o,c as r,a}from"./vendor.28ed3bb8.js";const l={setup(s){const t={}.VITE_CLIENT_ID,e={}.VITE_REDIRECT_URL,c=`https://bitly.com/oauth/authorize?client_id=${t}&redirect_uri=${e}`;return(_,n)=>(o(),r("section",null,[a("a",{href:c},"\u6388\u6B0A")]))}};export{l as default};
